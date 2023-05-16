f3d5d24953be5bb7346295a6830774bbf24cb3e0
parth-gr <paarora@redhat.com>
2023-05-16 20:10:29 +0530

    external: fix rgw check if no multisite flag is passed
    if there is no multisite config pass, it will still checks for
    the zones and zones group and not able to query anything
    
    Signed-off-by: parth-gr <paarora@redhat.com>
    (cherry picked from commit ec2681f82b0d9767283140113a73b79ce7740a13)

--- deploy/examples/create-external-cluster-resources.py
+++ deploy/examples/create-external-cluster-resources.py
@@ -1448,10 +1448,6 @@ class RadosJSON:
                 self.create_rgw_admin_ops_user()
             else:
                 if (
-                    self._arg_parser.rgw_realm_name == ""
-                    and self._arg_parser.rgw_zonegroup_name == ""
-                    and self._arg_parser.rgw_zone_name == ""
-                ) or (
                     self._arg_parser.rgw_realm_name != ""
                     and self._arg_parser.rgw_zonegroup_name != ""
                     and self._arg_parser.rgw_zone_name != ""
@@ -1465,6 +1461,16 @@ class RadosJSON:
                     err = self.validate_rgw_multisite(
                         self._arg_parser.rgw_zone_name, "zone"
                     )
+
+                if (
+                    self._arg_parser.rgw_realm_name == ""
+                    and self._arg_parser.rgw_zonegroup_name == ""
+                    and self._arg_parser.rgw_zone_name == ""
+                ) or (
+                    self._arg_parser.rgw_realm_name != ""
+                    and self._arg_parser.rgw_zonegroup_name != ""
+                    and self._arg_parser.rgw_zone_name != ""
+                ):
                     (
                         self.out_map["RGW_ADMIN_OPS_USER_ACCESS_KEY"],
                         self.out_map["RGW_ADMIN_OPS_USER_SECRET_KEY"],
